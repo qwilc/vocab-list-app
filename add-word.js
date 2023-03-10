df959654cbacea3ae6722bc1bb1355397c146d4b
Quincy Wilcox <qgwilcox@gmail.com>
2023-03-10 16:55:30 -0700

    Add functionality to saveWord() to store the word in localStorage

--- add-word.js
+++ add-word.js
@@ -1,7 +1,9 @@
-usernameEl = document.querySelector("#username")
-usernameEl.textContent = localStorage.getItem('username') ?? 'Anonymous';
+usernameEl = document.querySelector("#username");
+username = localStorage.getItem('username');
+usernameEl.textContent = username ?? 'Anonymous';
 
 function saveWord() {
+
     // Verify that there is something in the word box
 
     wordEl = document.querySelector("#word-input");
@@ -16,7 +18,31 @@ function saveWord() {
 
     new_word_json = JSON.stringify(new_word);
 
-    document.querySelector("#display").innerHTML = new_word_json;
+    stored_words = localStorage.getItem("words");
+    if(!stored_words) {
+        stored_words = {};
+    }
+    else {
+        stored_words = JSON.parse(stored_words);
+        console.log(stored_words)
+    }
+    
+    if(!stored_words[username]) {
+        stored_words[username] = [new_word];
+    }
+    else {
+        stored_words[username].push(new_word);
+    }
+
+    stored_words_json = JSON.stringify(stored_words);
+    localStorage.setItem("words", stored_words_json);
+    document.querySelector("#display").innerHTML = localStorage.getItem("words");
+    
+
+    // get existing storage 
+    // get existing storage for this user
+    // if it doesn't exist, just add this word in a list
+    // otherwise, add this word to the end of the list
 
 }
 
