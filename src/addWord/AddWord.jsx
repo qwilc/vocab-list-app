938c1c8591c17d99c8d141c564b7ef81ff64ba7b
qwilc <qgwilcox@gmail.com>
2024-08-17 21:16:45 -0600

    Lay out basic UI for Add Word page

--- src/addWord/AddWord.jsx
+++ src/addWord/AddWord.jsx
@@ -1,16 +1,26 @@
+import MyForm from "../sharedComponents/MyForm";
+import MyTextInput from "../sharedComponents/MyTextInput";
+import MyTagInput from "../sharedComponents/MyTagInput";
+import MyTextArea from "../sharedComponents/MyTextArea";
+
 function AddWord() {
 	return (
-		<main className="container-fluid text-center">
-			<div>
-				Current user: <span id="username">Anonymous</span>
-			</div>
-			<div>
-				Latest Activity: <span id="message"></span>
+		<>
+			<div className="container-fluid text-center">
+				<div>
+					Current user: <span id="username">Anonymous</span>
+				</div>
+				<div>
+					Latest Activity: <span id="message"></span>
+				</div>
 			</div>
 
-			<h1>Add New Word</h1>
-
-		</main>
+			<MyForm title="Add New Word">
+				<MyTextInput id="word-input" label="Word" placeholder="Enter word"></MyTextInput>
+				<MyTagInput id="word-tag-input" label="Tags"></MyTagInput>
+				<MyTextArea id="word-description-input" label="Description" placeholder="Type any notes or definitions here"></MyTextArea>
+			</MyForm>
+		</>
 	);
 }
 
